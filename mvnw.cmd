2cefd7de60658523b8f5f351cf3b448e7221b5c5
Voloirex <6276639+Voloire@users.noreply.github.com>
2026-09-01 14:06:49 +0200

    fix(T01): make Maven wrappers cwd independent

--- mvnw.cmd
+++ mvnw.cmd
@@ -1,4 +1,6 @@
 @echo off
 setlocal
- docker compose run --rm builder %*
-exit /b %ERRORLEVEL%
+set "SCRIPT_DIR=%~dp0."
+docker compose --project-directory "%SCRIPT_DIR%" -f "%SCRIPT_DIR%\compose.yaml" run --rm builder mvn %*
+set "EXIT_CODE=%ERRORLEVEL%"
+exit /b %EXIT_CODE%
